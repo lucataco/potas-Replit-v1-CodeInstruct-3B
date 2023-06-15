967c82fb19c74ed579e91dc9ebbecc7ce82022af
Luis <me@lucataco.com>
2023-06-15 01:59:52 -0400

    first changes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from potassium import Potassium, Request, Response
-
-from transformers import pipeline
 import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 app = Potassium("my_app")
 
@@ -9,23 +8,36 @@ app = Potassium("my_app")
 @app.init
 def init():
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
-   
-    context = {
-        "model": model
-    }
+    REPO = "teknium/Replit-v1-CodeInstruct-3B"
+    tokenizer = AutoTokenizer.from_pretrained(REPO, trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(REPO, torch_dtype=torch.bfloat16, trust_remote_code=True)
+    model.to(device)
 
+    context = {
+        "model": model,
+        "tokenizer": tokenizer
+    }
     return context
+
 
 # @app.handler runs for every call
 @app.handler()
 def handler(context: dict, request: Request) -> Response:
     prompt = request.json.get("prompt")
+    max_tokens = request.json.get("max_tokens", 128)
+    temperature = request.json.get("temperature", 0.7)
     model = context.get("model")
-    outputs = model(prompt)
+    tokenizer = context.get("tokenizer")
+
+    top_p=0.9 
+    eos_token_id=tokenizer.eos_token_id
+
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
+    generated_ids = model.generate(input_ids, max_new_tokens=max_tokens, do_sample=True, use_cache=True, temperature=temperature, top_p=top_p, eos_token_id=eos_token_id)
+    output = tokenizer.decode(generated_ids[0][input_ids.shape[-1]:], skip_special_tokens=True, clean_up_tokenization_spaces=False)
 
     return Response(
-        json = {"outputs": outputs[0]}, 
+        json = {"response": output}, 
         status=200
     )
 
--- download.py
+++ download.py
@@ -1,11 +1,13 @@
 # This file runs during container build time to get model weights built into the container
 
 # In this example: A Huggingface BERT model
-from transformers import pipeline
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    AutoTokenizer.from_pretrained("teknium/Replit-v1-CodeInstruct-3B", trust_remote_code=True)
+    AutoModelForCausalLM.from_pretrained("teknium/Replit-v1-CodeInstruct-3B", torch_dtype=torch.bfloat16, trust_remote_code=True)
 
 if __name__ == "__main__":
     download_model()
